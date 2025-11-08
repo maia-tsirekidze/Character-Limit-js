2bfd341b10c1b317d0cf94985e4e58ab1503f33b
maia-tsirekidze <115072030+maia-tsirekidze@users.noreply.github.com>
2025-11-09 01:36:14 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Character Limit
+// In this challenge, you are given a string and need to determine if it fits in a social media post. Return the following strings based on the rules given:
+
+// "short post" if it fits within a 40-character limit.
+// "long post" if it's greater than 40 characters and fits within an 80-character limit.
+// "invalid post" if it's too long to fit within either limit.
+function canPost(message) {
+   if (message.length<=40){
+     return "short post"
+   }else if(message.length>=40&&message.length<=80){
+     return "long post"
+   }else{
+     return "invalid post"
+   }
+  return message;
+}
+console.log(canPost("This message is too long to fit into either of the character limits for a social media post."))
